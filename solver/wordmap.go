3fc438fd609a3c1c97952b42517c73a70c279a83
Lucas Kimber <lucas.kimber@outlook.com>
2024-12-30 17:21:33 +0000

    Added initial code for map wrapper
    Defines a map wrapper that handles building a map where keys are an ordered set of letters in the item added.

---
+++ solver/wordmap.go
@@ -0,0 +1,37 @@
+package solver
+
+import (
+	"sort"
+	"strings"
+)
+
+type wordMap struct {
+	store map[string][]string
+}
+
+func NewWordMap() *wordMap {
+	return &wordMap{map[string][]string{}}
+}
+
+func makeKey(word string) string {
+
+	arr := strings.Split(word, "")
+	setMap := make(map[string]bool)
+
+	for _, i := range arr {
+		setMap[i] = true
+	}
+
+	setSlice := []string{}
+
+	for k := range setMap {
+		setSlice = append(setSlice, k)
+	}
+
+	return strings.Join(sort.StringSlice(setSlice), "")
+}
+
+func (dict wordMap) AddWord(newWord string) {
+
+	dict.store[makeKey(newWord)] = append(dict.store[makeKey(newWord)], newWord)
+}
