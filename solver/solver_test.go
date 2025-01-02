8f6d496c0a6a3a9534acd3f6c7aebb141386b58a
Lucas Kimber <lucas.kimber@outlook.com>
2025-01-02 16:21:34 +0000

    Added more test cases for the solver

--- solver/solver_test.go
+++ solver/solver_test.go
@@ -13,3 +13,42 @@ func TestFindSubsets(t *testing.T) {
 		t.Errorf("findSubsets(\"abcdef\") = %s; want %s", got, want)
 	}
 }
+
+func TestSolve(t *testing.T) {
+
+	s := NewSolver()
+	s.ParseWords([]string{"abcd", "bcad", "bacd", "xyzd"})
+
+	got := s.Solve("a", "bcdefg")
+	want := []string{"abcd", "bcad", "bacd"}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("ParseWords([]string{\"abcd\", \"bcad\", \"bacd\", \"xyzd\"}) then Solve(\"a\", \"abcdefg\") = %s; want %s", got, want)
+	}
+}
+
+func TestSolveRejectsWithoutCentreLetter(t *testing.T) {
+
+	s := NewSolver()
+	s.ParseWords([]string{"bcde", "cbde", "abcd"})
+
+	got := s.Solve("a", "bcdefg")
+	want := []string{"abcd"}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("ParseWords([]string{\"bcde\", \"cbde\", \"abcd\"}) then Solve(\"a\", \"abcd\") = %s; want %s", got, want)
+	}
+}
+
+func TestSolveSubsets(t *testing.T) {
+
+	s := NewSolver()
+	s.ParseWords([]string{"abcc", "abccdd", "aaaa"})
+
+	got := s.Solve("a", "abcdef")
+	want := []string{"abcc", "abccdd", "aaaa"}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("ParseWords([]string{\"abcc\", \"abccdd\", \"aaaa\"}) then Solve(\"a\", \"abcdef\") = %s; want %s", got, want)
+	}
+}
