5c398cef9a447128b7dea9a99d465df65d149c93
Lucas Kimber <lucas.kimber@outlook.com>
2025-01-04 03:12:58 +0000

    Added comment

--- dictionary/dictparser.go
+++ dictionary/dictparser.go
@@ -5,38 +5,8 @@ import (
 	"os"
 )
 
-// type jsonStructure struct {
-// 	field []string
-// }
-
-// func ParseJSON(path string) ([]string, error) {
-
-// 	parsedWords := []string{}
-
-// 	file, err := os.Open(path)
-
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	defer file.Close()
-
-// 	decoder := json.NewDecoder(file)
-
-// 	for decoder.More() {
-
-// 		var entry jsonStructure
-
-// 		if err := decoder.Decode(&entry); err != nil {
-// 			return nil, err
-// 		}
-
-// 		parsedWords = append(parsedWords, entry.field[1])
-// 	}
-
-// 	return parsedWords, nil
-// }
-
+// ParseJSON takes a path to a dictionary in JSON format and parses it into a slice of strings.
+// It assumes the field name for the words in the dictionary to be "word".
 func ParseJSON(path string) ([]string, error) {
 	parsedWords := []string{}
 
@@ -57,6 +27,7 @@ func ParseJSON(path string) ([]string, error) {
 	}
 
 	for decoder.More() {
+
 		var entry struct {
 			Word string `json:"word"`
 		}
