525080aff376ad90293ee92a2ecc7cc5872e4060
Lucas Kimber <lucas.kimber@outlook.com>
2025-01-04 00:05:01 +0000

    Added documentation

--- solver/solver.go
+++ solver/solver.go
@@ -5,14 +5,17 @@ import (
 	"strings"
 )
 
+// solver wraps a wordMap and is used to implement functionality for actually solving SpellingBee given a problem set.
 type solver struct {
 	dictionary wordMap
 }
 
+// NewSolver constructs a solver with an initialised wordMap
 func NewSolver() *solver {
 	return &solver{*NewWordMap()}
 }
 
+// ParseWords takes a slice of strings and adds them to the wordMap
 func (s solver) ParseWords(words []string) {
 
 	for _, w := range words {
@@ -20,6 +23,7 @@ func (s solver) ParseWords(words []string) {
 	}
 }
 
+// findSubsets takes a string and generates all unique permutations of its letters.
 func findSubsets(s string) []string {
 
 	n := len(s)
@@ -43,6 +47,7 @@ func findSubsets(s string) []string {
 	return subsets
 }
 
+// insertInOrder takes a string a character, then returns a string with the character added in order.
 func insertInOrder(c string, str string) string {
 
 	str += c
@@ -53,6 +58,12 @@ func insertInOrder(c string, str string) string {
 	return strings.Join(sortedStr, "")
 }
 
+// Solve takes a SpellingBee problem instance and returns a slice containing all valid words.
+// The problem set is given as:
+// centreLetter, the letter that must be present in all solution words
+// inputLetters, the set of letters that can be used to make words
+// It then finds all unique permutations of inputLetters, adds the centreLetter, and looks up the words that can be made from these permutations in the wordMap.
+// The return is a string slice which is the uninion of all the lookups.
 func (s solver) Solve(centreLetter string, inputLetters string) []string {
 
 	letterSet := ""
--- solver/wordmap.go
+++ solver/wordmap.go
@@ -6,14 +6,18 @@ import (
 	"unicode"
 )
 
+// wordMap wraps a map structure and stores ordered unique letter sets with corresponding words.
+// It is used for grouping words that can be constructed from the same letters.
 type wordMap struct {
 	store map[string][]string
 }
 
+// NewWordMap constructs an empty wordMap.
 func NewWordMap() *wordMap {
 	return &wordMap{map[string][]string{}}
 }
 
+// makeKey takes a word as a string and returns a string consisting of all the unique letters present and in order.
 func makeKey(word string) string {
 
 	word = strings.ToLower(word)
@@ -39,11 +43,15 @@ func makeKey(word string) string {
 	return string(setSlice)
 }
 
+// AddWord adds a new word to the wordMap.
+// Words added are stored under a key consisting of the set of unique letters in the word in order.
 func (wm wordMap) AddWord(newWord string) {
 
 	wm.store[makeKey(newWord)] = append(wm.store[makeKey(newWord)], newWord)
 }
 
+// Lookup returns all words in the wordMap that have the same key.
+// Practically speaking this means returning all words that can be constructed from the same set of letters.
 func (wm wordMap) Lookup(letterSet string) []string {
 	return wm.store[letterSet]
 }
